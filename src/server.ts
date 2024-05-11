2ad606ce6fcecd6b64aa3410018a46c0f0bea684
Pablo Camacho <pablocamacho92@gmail.com>
2024-05-12 01:03:41 +0200

    Crear planta, agregar alertas a las plantas, traer alertas por tipo de alerta

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,6 @@ import jwt from 'jsonwebtoken';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-
-
 const prisma = new PrismaClient();
 const app = express();
 const port = 3000;
@@ -46,6 +44,7 @@ app.post('/register', async (req, res) => {
       data: {
         email,
         passswordHash: hashedPassword,
+        updatedAt: new Date(),
       },
     });
 
@@ -53,12 +52,6 @@ app.post('/register', async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: 'Ocurrió un error al registrar al usuario' });
   }
-});
-
-
-// Ruta que devuelve un JSON
-app.get('/api', (req, res) => {
-  res.json({ mensaje: '¡Hola, mundo!' });
 });
 
 app.post('/login', async (req, res) => {
@@ -69,29 +62,113 @@ app.post('/login', async (req, res) => {
     if (!user || !(await bcrypt.compare(password, user.passswordHash))) {
       return res.status(401).json({ message: 'Email o contraseña invalida' });
     }
-
-    const token = jwt.sign({ id: user.id, email: user.email }, "process.env.JWT_SECRET_KEY");
+    if (!process.env.JWT_SECRET_KEY) {
+      throw new Error('La llave secreta de JWT no esta definida');
+    }
+    const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET_KEY);
     res.json({ token });
   } catch (error) {
     res.status(500).json({ message: 'Ocurrió un error al intentar iniciar sesión' });
   }
 });
 
+app.get('/alerts/:type', async (req, res) => {
+  const { type } = req.params;
+  const alertType = await prisma.alertType.findUnique({
+    where: {
+      tipo: type,
+    },
+  });
 
-//Ejemplo de proteccion de endpoints
-app.get('/me', async (req, res) => {
-  const authHeader = req.headers.authorization;
-  const token = authHeader?.split(' ')[1];
-
-  if (!token) {
-    return res.status(401).json({ message: 'Falta token de autenticación' });
+  if (!alertType) {
+    return res.status(404).json({ error: 'Tipo de alerta no encontrado' });
   }
 
+  const alerts = await prisma.alert.findMany({
+    where: {
+      alertTypeId: alertType.id,
+    },
+    include: {
+      alertSeverity: true,
+    },
+  });
+
+  const severityCounts = alerts.reduce((counts: { [key: string]: number }, alert) => {
+    if (!counts[alert.alertSeverity.severity]) {
+      counts[alert.alertSeverity.severity] = 0;
+    }
+    counts[alert.alertSeverity.severity]++;
+    return counts;
+  }, {});
+
+  res.json(severityCounts);
+});
+
+
+app.post('/plants', async (req, res) => {
+  const { name, country } = req.body;
+
+  const newPlant = await prisma.plant.create({
+      data: {
+          name: name,
+          country: country,
+      },
+  });
+
+  res.json(newPlant);
+});
+
+app.post('/createAlerts', async (req, res) => {
+  const { name, country, alertSeverities } = req.body;
+
   try {
-    const payload = jwt.verify(token, 'Key Secreta');
-    res.json(payload);
-  } catch (e) {
-    return res.status(401).json({ message: 'Token invalido o expirado' });
+    const plant = await prisma.plant.findUnique({
+      where: {
+        name,
+      },
+    });
+
+    if (!plant) {
+      return res.status(400).json({ error: `La planta ${name} no existe` });
+    }
+
+    const alertTypes = await prisma.alertType.findMany();
+
+    await prisma.alert.deleteMany({
+      where: {
+        plantId: plant.id,
+      },
+    });
+
+    for (let alertSeverity of alertSeverities) {
+      const { severity, count } = alertSeverity;
+
+      const alertSeverityRecord = await prisma.alertSeverity.findUnique({
+        where: {
+          severity,
+        },
+      });
+
+      if (!alertSeverityRecord) {
+        return res.status(400).json({ error: `AlertSeverity ${severity} does not exist` });
+      }
+
+      for (let i = 0; i < count; i++) {
+        const randomAlertType = alertTypes[Math.floor(Math.random() * alertTypes.length)];
+
+        await prisma.alert.create({
+          data: {
+            plantId: plant.id,
+            alertTypeId: randomAlertType.id,
+            alertSeverityId: alertSeverityRecord.id,
+          },
+        });
+      }
+    }
+
+    res.status(200).json({ message: 'Alertas creadas correctamente' });
+  } catch (error) {
+    res.status(500).json({ error: 'Ocurrio un error en la creacion de alertas' });
   }
 });
 
