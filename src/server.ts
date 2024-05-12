3dfe016b5095f49ae19acc4f4aa953be59b9d2d8
Pablo <pablocamacho92@gmail.com>
2024-05-12 16:56:40 -0300

    Edito login

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import express from 'express';
-import { PrismaClient } from '@prisma/client';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import * as dotenv from 'dotenv';
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import * as dotenv from "dotenv";
 dotenv.config();
 
 const prisma = new PrismaClient();
@@ -11,7 +11,7 @@ const port = 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send(`
     <html>
       <body>
@@ -53,7 +53,7 @@ app.get('/', (req, res) => {
   `);
 });
 
-app.post('/register', async (req, res) => {
+app.post("/register", async (req, res) => {
   try {
     const { email, password } = req.body;
     const existingUser = await prisma.user.findUnique({
@@ -61,15 +61,20 @@ app.post('/register', async (req, res) => {
         email: email,
       },
     });
-    const emailRegularExpression = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+    const emailRegularExpression =
+      /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
     if (!emailRegularExpression.test(email)) {
-      return res.status(400).json({ error: 'El email es invalido' });
+      return res.status(400).json({ error: "El email es invalido" });
     }
     if (password.length < 8) {
-      return res.status(400).json({ error: 'La contraseña debe tener al menos 8 dígitos' });
+      return res
+        .status(400)
+        .json({ error: "La contraseña debe tener al menos 8 dígitos" });
     }
     if (existingUser) {
-      return res.status(400).json({ error: 'El correo electrónico ya está registrado' });
+      return res
+        .status(400)
+        .json({ error: "El correo electrónico ya está registrado" });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await prisma.user.create({
@@ -82,29 +87,42 @@ app.post('/register', async (req, res) => {
 
     res.json(user);
   } catch (error) {
-    res.status(500).json({ error: 'Ocurrió un error al registrar al usuario' });
+    res.status(500).json({ error: "Ocurrió un error al registrar al usuario" });
   }
 });
 
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await prisma.user.findUnique({ where: { email } });
 
     if (!user || !(await bcrypt.compare(password, user.passswordHash))) {
-      return res.status(401).json({ message: 'Email o contraseña invalida' });
+      return res.status(401).json({ message: "Email o contraseña invalida" });
     }
     if (!process.env.JWT_SECRET_KEY) {
-      throw new Error('La llave secreta de JWT no esta definida');
+      throw new Error("La llave secreta de JWT no esta definida");
     }
-    const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET_KEY);
-    res.json({ token });
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET_KEY
+    );
+    res.json({
+      token,
+      user: {
+        id: user.id,
+        email: user.email,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt,
+      },
+    });
   } catch (error) {
-    res.status(500).json({ message: 'Ocurrió un error al intentar iniciar sesión' });
+    res
+      .status(500)
+      .json({ message: "Ocurrió un error al intentar iniciar sesión" });
   }
 });
 
-app.get('/alerts/:type', async (req, res) => {
+app.get("/alerts/:type", async (req, res) => {
   const { type } = req.params;
   const alertType = await prisma.alertType.findUnique({
     where: {
@@ -113,7 +131,7 @@ app.get('/alerts/:type', async (req, res) => {
   });
 
   if (!alertType) {
-    return res.status(404).json({ error: 'Tipo de alerta no encontrado' });
+    return res.status(404).json({ error: "Tipo de alerta no encontrado" });
   }
 
   const alerts = await prisma.alert.findMany({
@@ -125,32 +143,34 @@ app.get('/alerts/:type', async (req, res) => {
     },
   });
 
-  const severityCounts = alerts.reduce((counts: { [key: string]: number }, alert) => {
-    if (!counts[alert.alertSeverity.severity]) {
-      counts[alert.alertSeverity.severity] = 0;
-    }
-    counts[alert.alertSeverity.severity]++;
-    return counts;
-  }, {});
+  const severityCounts = alerts.reduce(
+    (counts: { [key: string]: number }, alert) => {
+      if (!counts[alert.alertSeverity.severity]) {
+        counts[alert.alertSeverity.severity] = 0;
+      }
+      counts[alert.alertSeverity.severity]++;
+      return counts;
+    },
+    {}
+  );
 
   res.json(severityCounts);
 });
 
-
-app.post('/plants', async (req, res) => {
+app.post("/plants", async (req, res) => {
   const { name, country } = req.body;
 
   const newPlant = await prisma.plant.create({
-      data: {
-          name: name,
-          country: country,
-      },
+    data: {
+      name: name,
+      country: country,
+    },
   });
 
   res.json(newPlant);
 });
 
-app.post('/createAlerts', async (req, res) => {
+app.post("/createAlerts", async (req, res) => {
   const { name, country, alertSeverities } = req.body;
 
   try {
@@ -182,11 +202,14 @@ app.post('/createAlerts', async (req, res) => {
       });
 
       if (!alertSeverityRecord) {
-        return res.status(400).json({ error: `AlertSeverity ${severity} does not exist` });
+        return res
+          .status(400)
+          .json({ error: `AlertSeverity ${severity} does not exist` });
       }
 
       for (let i = 0; i < count; i++) {
-        const randomAlertType = alertTypes[Math.floor(Math.random() * alertTypes.length)];
+        const randomAlertType =
+          alertTypes[Math.floor(Math.random() * alertTypes.length)];
 
         await prisma.alert.create({
           data: {
@@ -198,13 +221,15 @@ app.post('/createAlerts', async (req, res) => {
       }
     }
 
-    res.status(200).json({ message: 'Alertas creadas correctamente' });
+    res.status(200).json({ message: "Alertas creadas correctamente" });
   } catch (error) {
-    res.status(500).json({ error: 'Ocurrio un error en la creacion de alertas' });
+    res
+      .status(500)
+      .json({ error: "Ocurrio un error en la creacion de alertas" });
   }
 });
 
-app.get('/alerts/severity/:severity', async (req, res) => {
+app.get("/alerts/severity/:severity", async (req, res) => {
   const { severity } = req.params;
 
   const alertSeverity = await prisma.alertSeverity.findUnique({
@@ -214,7 +239,7 @@ app.get('/alerts/severity/:severity', async (req, res) => {
   });
 
   if (!alertSeverity) {
-    return res.status(404).json({ error: 'Severidad de alerta no encontrada' });
+    return res.status(404).json({ error: "Severidad de alerta no encontrada" });
   }
 
   const alerts = await prisma.alert.findMany({
@@ -226,7 +251,7 @@ app.get('/alerts/severity/:severity', async (req, res) => {
   res.json({ count: alerts.length });
 });
 
-app.get('/plants/alerts', async (req, res) => {
+app.get("/plants/alerts", async (req, res) => {
   try {
     const plants = await prisma.plant.findMany({
       include: {
@@ -239,13 +264,16 @@ app.get('/plants/alerts', async (req, res) => {
     });
 
     const plantAlerts = plants.map((plant) => {
-      const severityCounts = plant.alerts.reduce((counts: Record<string, number>, alert) => {
-        if (!counts[alert.alertSeverity.severity]) {
-          counts[alert.alertSeverity.severity] = 0;
-        }
-        counts[alert.alertSeverity.severity]++;
-        return counts;
-      }, {} as Record<string, number>);
+      const severityCounts = plant.alerts.reduce(
+        (counts: Record<string, number>, alert) => {
+          if (!counts[alert.alertSeverity.severity]) {
+            counts[alert.alertSeverity.severity] = 0;
+          }
+          counts[alert.alertSeverity.severity]++;
+          return counts;
+        },
+        {} as Record<string, number>
+      );
 
       return {
         name: plant.name,
@@ -257,11 +285,11 @@ app.get('/plants/alerts', async (req, res) => {
 
     res.json(plantAlerts);
   } catch (error) {
-    res.status(500).json({ error: 'An error occurred while fetching alerts' });
+    res.status(500).json({ error: "An error occurred while fetching alerts" });
   }
 });
 
-app.delete('/plants/:name', async (req, res) => {
+app.delete("/plants/:name", async (req, res) => {
   const { name } = req.params;
 
   const plant = await prisma.plant.findUnique({
@@ -271,7 +299,7 @@ app.delete('/plants/:name', async (req, res) => {
   });
 
   if (!plant) {
-    return res.status(404).json({ error: 'Planta no encontrada' });
+    return res.status(404).json({ error: "Planta no encontrada" });
   }
   await prisma.alert.deleteMany({
     where: {
@@ -284,7 +312,7 @@ app.delete('/plants/:name', async (req, res) => {
     },
   });
 
-  res.json({ message: 'Planta y sus alertas eliminadas con éxito' });
+  res.json({ message: "Planta y sus alertas eliminadas con éxito" });
 });
 
 app.listen(port, () => {
